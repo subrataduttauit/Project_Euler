a3606c88b4bf15938c55c1b166e595623332c9e4
subrataduttauit <subrataduttauit@gmail.com>
2015-08-21 00:26:35 +0530

    solution

---
+++ euler27.cpp
@@ -0,0 +1,54 @@
+/* 
+ * Solution to Project Euler problem 27
+ * by Subrata Dutta
+ * 
+ * University Institute of Technology, Burdwan University
+ * Dept. of Information Technology
+ * https://github.com/subrataduttauit
+ * https://in.linkedin.com/in/subrataduttauit
+ */
+
+
+#include <stdio.h>
+#include <math.h>
+
+static int is_prime(int n);
+
+int main(void)
+{
+  int a, b, n;
+  int nmax = 0, amax = 0, bmax = 0;
+
+  for (a = -999; a < 1000; a++) {
+    for (b = -999; b < 1000; b++) {
+      for (n = 0; is_prime(n*n + a*n + b); n++);
+      if (n > nmax) {
+        nmax = n;
+        amax = a;
+        bmax = b;
+      }
+    }
+  }
+  printf("%d\n", amax * bmax);
+
+  return 0;
+}
+
+int is_prime(int n)
+{
+  int i;
+
+  if (n <= 1) {
+    return 0;
+  }
+  if (n == 2) {
+    return 1;
+  }
+
+  for (i = 2; i < sqrt((double)n); i++) {
+    if (n % i == 0) {
+      return 0;
+    }
+  }
+  return 1;
+}
