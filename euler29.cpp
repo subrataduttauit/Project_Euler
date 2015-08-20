688a74c92eef833f17da1f328eae4e600586a069
subrataduttauit <subrataduttauit@gmail.com>
2015-08-21 00:28:17 +0530

    solution

---
+++ euler29.cpp
@@ -0,0 +1,63 @@
+/* 
+ * Solution to Project Euler problem 29
+ * by Subrata Dutta
+ * 
+ * University Institute of Technology, Burdwan University
+ * Dept. of Information Technology
+ * https://github.com/subrataduttauit
+ * https://in.linkedin.com/in/subrataduttauit
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
+
+#define N 100
+
+static int compare(const void *a, const void *b);
+
+int main(void)
+{
+  int a, b;
+  int i = 0;
+  int count = 0;
+  mpz_t *nums;
+  mpz_t t;
+
+  mpz_init(t);
+
+  nums = malloc((N-1)*(N-1) * sizeof *nums);
+  for (a = 2; a <= N; a++) {
+    mpz_set_ui(t, a);
+    for (b = 2; b <= N; b++) {
+      mpz_mul_ui(t, t, a);
+      mpz_init(nums[i]);
+      mpz_set(nums[i], t);
+      i++;
+    }
+  }
+
+  qsort(nums, (N-1)*(N-1), sizeof *nums, compare);
+
+  mpz_set_ui(t, 0);
+  for (i = 0; i < (N-1)*(N-1); i++) {
+    if (mpz_cmp(t, nums[i]) != 0) {
+      count++;
+    }
+    mpz_set(t, nums[i]);
+    mpz_clear(nums[i]);
+  }
+  printf("%d\n", count);
+
+  free(nums);
+  mpz_clear(t);
+
+  return 0;
+}
+
+int compare(const void *a, const void *b)
+{
+  return mpz_cmp(*(const mpz_t *)a, *(mpz_t const *)b);
+}
+
