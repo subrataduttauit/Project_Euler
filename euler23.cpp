73a2f8776da02a27ac10375ab925b657e6eaa36f
subrataduttauit <subrataduttauit@gmail.com>
2015-08-20 23:34:18 +0530

    solution

---
+++ euler23.cpp
@@ -0,0 +1,77 @@
+/* 
+ * Solution to Project Euler problem 23
+ * by Subrata Dutta
+ * 
+ * University Institute of Technology, Burdwan University
+ * Dept. of Information Technology
+ * https://github.com/subrataduttauit
+ * https://in.linkedin.com/in/subrataduttauit
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static int is_abundant(int n);
+
+int main(void)
+{
+  int i, j;
+  const int upper_limit = 28123;
+  int *abundants, abundants_count = 0;
+  char *expressed;
+  int sum = 0;
+
+  for (i = 1; i < upper_limit; i++) {
+    if (is_abundant(i)) {
+      abundants_count++;
+    }
+  }
+
+  abundants = malloc(abundants_count * sizeof *abundants);
+  j = 0;
+  for (i = 1; i < upper_limit; i++) {
+    if (is_abundant(i)) {
+      abundants[j++] = i;
+    }
+  }
+
+  expressed = calloc(upper_limit, sizeof *expressed);
+  for (i = 0; i < abundants_count; i++) {
+    for (j = i; j < abundants_count; j++) {
+      int k = abundants[i] + abundants[j];
+      if (k >= upper_limit) {
+        break;
+      }
+      expressed[k-1] = 1;
+    }
+  }
+
+  for (i = 1; i < upper_limit; i++) {
+    if (!expressed[i-1]) {
+      sum += i;
+    }
+  }
+  printf("%d\n", sum);
+
+  free(abundants);
+  free(expressed);
+
+  return 0;
+}
+
+int is_abundant(int n)
+{
+  int sum = 1;
+  int i, k = n;
+  for (i = 2; i <= k; i++) {
+    int p = 1;
+    while (k % i == 0) {
+      p *= i;
+      k /= i;
+    } 
+    sum *= (p*i - 1)/(i-1);
+  }
+  return 2*n < sum;
+}
+
