83ad2a39ad50620de6c125119936c1a89585155a
subrataduttauit <subrataduttauit@gmail.com>
2015-08-25 00:27:22 +0530

    solution

---
+++ euler46.cpp
@@ -0,0 +1,80 @@
+/* 
+ * Solution to Project Euler problem 46
+ * by Subrata Dutta
+ * 
+ * University Institute of Technology, Burdwan University
+ * Dept. of Information Technology
+ * https://github.com/subrataduttauit
+ * https://in.linkedin.com/in/subrataduttauit
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define N 10000
+
+static int is_square(unsigned n);
+static char *prime_sieve(unsigned n);
+
+int main(void)
+{
+  char *sieve = prime_sieve(N);
+  unsigned *primes = malloc(sizeof(unsigned) * N);
+  unsigned i, j = 0;
+
+  for (i = 0; i < N; i++) {
+    if (!sieve[i]) {
+      primes[j++] = i;
+    }
+  }
+  primes[j] = 0;
+
+  for (i = 3; i < N; i += 2) {
+    if (!sieve[i]) {
+      /* skip if i is prime */
+      continue;
+    }
+    for (j = 0; primes[j]; j++) {
+      unsigned s;
+
+      if (primes[j] > i) {
+        printf("%u\n", i);
+        goto FINISH;
+      }
+
+      s = (i - primes[j])/2;
+      if (is_square(s)) {
+        break;
+      }
+    }
+  }
+FINISH:
+  free(sieve);
+  free(primes);
+
+  return 0;
+}
+
+char *prime_sieve(unsigned n)
+{
+  char *sieve = calloc(n, sizeof *sieve);
+  unsigned i;
+  sieve[0] = sieve[1] = 1;
+  for (i = 2; i < n; i++) {
+    if (!sieve[i]) {
+      unsigned j;
+      for (j = i*2; j < n; j += i) {
+        sieve[j] = 1;
+      }
+    }
+  }
+  return sieve;
+}
+
+int is_square(unsigned n)
+{
+  unsigned sq = sqrt(n);
+  return sq*sq == n;
+}
